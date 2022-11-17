2d9f8155fae4236363ab3930cc6f2910763c55a9
maxc0der <maximka5406@gmail.com>
2022-11-17 17:06:03 +0300

    update

--- VK_API.py
+++ VK_API.py
@@ -1,7 +1,7 @@
 """
 Класс для работы с VK-API
 Create at 28.09.2022 19:22:47
-~/main.py
+~/vk.py
 """
 
 __author__ = 'maxc0der'
@@ -15,8 +15,9 @@ __status__ = "Production"
 
 
 import requests
-import time
+import time, datetime
 import json
+from typing import List, Optional, Dict, Iterator
 
 
 class VK:
@@ -41,7 +42,7 @@ class VK:
         """get answer from VK API"""
         response = requests.get(
             'https://api.vk.com/method/' + method,
-            params={'access_token': self.token, 'v': version} | request,
+            params={**{'access_token': self.token, 'v': version}, **request},
             headers={'Accept': 'application/json', "User-Agent": "curl/7.61.0"},
         )
         r = response.content.decode()
@@ -51,7 +52,9 @@ class VK:
         else:
             return response_json['error']['error_msg']
 
-    def get_posts_filtered_by_date(self, source_id, start_date, end_date):
+    def get_posts_filtered_by_date(self, source_id, start_date: str, end_date: str):
+        start_date = VK.date_formatter(str(start_date))
+        end_date = VK.date_formatter(str(end_date))
         """Return json-object of posts from source_id with date between start_date and end_date"""
         result = list()
         offset = 0
@@ -67,7 +70,8 @@ class VK:
                 if response['items'][-1]['date'] < end_date:
                     for item in items:
                         if end_date > item['date'] > start_date:
-                            result.append(item)
+                            #result.append(item)
+                            yield item
                 if items_count == -1:
                     items_count = response['count']
                 if offset >= items_count:
@@ -78,3 +82,26 @@ class VK:
             time.sleep(0.5)
 
         return result
+
+    def vk_group_load(
+            self,
+            source_id: str,  # channel
+            start_date: datetime.datetime,
+            stop_date: datetime.datetime,
+    ) -> Iterator[Dict]:
+
+        start_date = time.mktime(start_date.timetuple())
+        stop_date = time.mktime(stop_date.timetuple())
+        posts = self.get_posts_filtered_by_date(source_id, start_date, stop_date)
+        # async for tmessage in iterator:
+        # TODO: enrichment_and_castration (check tg-extractor) !!!
+        for vk_post in posts:
+            yield vk_post
+        pass
+
+    @staticmethod
+    def date_formatter(x):
+        """Input YYYYMMDD date, return Unix-format"""
+        return time.mktime(datetime.date(int(x[0:4]), int(x[4:6]), int(x[6:8])).timetuple())
+
+
